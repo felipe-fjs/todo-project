c5b75fe3fc8e988984a3e423fea6c624efd60900
felipe-fjs <167233033+felipe-fjs@users.noreply.github.com>
2024-08-21 11:24:14 -0300

    update signup.html
    add formulário pelo WTForms

--- app/models/user.py
+++ app/models/user.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(100, unique=True, nullable=False))
+    email = db.Column(db.String(100), unique=True, nullable=False)
     pwd = db.Column(db.String(128), nullable=False)
     email_confirmed = db.Column(db.Boolean)
 
